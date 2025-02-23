88175f86b3f0b56cd3a05c8879eeb054e89d4396
maddaliarchana <archanasreekanth25@gmail.com>
2025-02-23 15:57:44 +0530

    install react vite

---
+++ amazon/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <nav className='navbar  fixed-bottom navbar-expand-lg bg-secondary-subtle'>
+            footer
+        </nav>
+    </>
+  )
+}
+
+export default Footer
---
+++ amazon/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from './Footer'
+import Header from './Header'
+
+const Home = () => {
+  return (
+    <>
+    <Header/>
+        test amazon site
+    <Footer/>
+    </>
+  )
+}
+
+export default Home
